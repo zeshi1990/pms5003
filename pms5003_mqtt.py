af5c52f41c624701da5cbef298ce6c5a55bdabb9
zeshi1990 <zeshi.z@gmail.com>
2018-05-22 17:25:05 -0700

    pms5003 driver

---
+++ pms5003_mqtt.py
@@ -0,0 +1,23 @@
+import time
+from pms5003_driver import PMS5003
+from awsiotconn import MQTTCONN
+
+def main():
+    sensor = PMS5003(port="/dev/ttyO1", baudrate=9600)
+    sensor.initialize()
+
+    awshost = "data.iot.us-east-2.amazonaws.com"
+    awsport = 8883
+    clientId = "pm25Sensor"
+    topic = "pm25"
+    thingName = "pm25Sensor"
+    caPath = "cert/aws-iot-rootCA.crt"
+    certPath = "cert/cert.pem"
+    keyPath = "cert/privkey.pem"
+
+    mqttconn = MQTTCONN(awshost, awsport, topic, caPath, certPath, keyPath, listener=False)
+
+    while True:
+        data = sensor.read_passive()
+        print data
+        time.sleep(2)
